0a2eb20a05b4043eab516a72b8039169256ce599
一个小阚阚 <little-kan@qq.com>
2023-05-01 08:41:50 +0000

    FINAL! add /lkmenu command add aliases add permissions modify IDEA Minecraft Dev Plugin IML file name add default config file

--- src/main/java/ml/littlekan/lkmenu/LKMenu.java
+++ src/main/java/ml/littlekan/lkmenu/LKMenu.java
@@ -1,6 +1,9 @@
 package ml.littlekan.lkmenu;
 
+import java.io.File;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import io.papermc.lib.PaperLib;
 
 public final class LKMenu extends JavaPlugin {
     private java.util.logging.Logger logger = getLogger();
@@ -8,22 +11,28 @@ public final class LKMenu extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        logger.info("ChestMenu 1.0.0, by @littlekan233");
+        logger.info("LKMenu 1.0.0, by @littlekan233");
         logger.info("Hello, server owner! >w<");
         if(!getDataFolder().isDirectory()){
             logger.info("It seems like you're first of use this plugin, initializing...");
+            /** Lazy to delete
             getConfig().set("default-width", 7);
             getConfig().set("default-height", 5);
             getConfig().set("enabled-menu",new String[]{});
             saveConfig();
+             **/
+            new File(getDataFolder(), "menus").mkdir();
+            saveDefaultConfig();
         }
         logger.info("Registering commands & event listeners...");
         getCommand("openmenu").setExecutor(new OpenCommand());
+        getCommand("lkmenu").setExecutor(new MainCommand());
         getServer().getPluginManager().registerEvents(new MenuEvent(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        logger.info("See you next time");  
     }
 }
---
+++ src/main/java/ml/littlekan/lkmenu/MainCommand.java
@@ -0,0 +1,44 @@
+package ml.littlekan.lkmenu;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        JavaPlugin instance = JavaPlugin.getPlugin(LKMenu.class);
+        if(args[0].toLowerCase() == "open"){
+            if(sender.hasPermission("lkmenu.open")){
+                new OpenCommand().onCommand(sender, command, "openmenu", new String[]{args[1]});
+            }else{
+                sender.sendMessage("§r[§cLKMenu§r] §cYou don't have permission to do that!");
+            }
+        }else if(args[0].toLowerCase() == "reload"){
+            if(sender.hasPermission("lkmenu.reload")){
+                instance.reloadConfig();
+                sender.sendMessage("§a[§6LKMenu§a] Reload success!");
+            }else{
+                sender.sendMessage("§r[§cLKMenu§r] §cYou don't have permission to do that!");
+            }
+        }else if(args.length == 0 || args[0] == "version"){
+            sender.sendMessage("§aServer is running §6LKMenu §bv1.0.0§a! <3");
+            sender.sendMessage("§aType §6§l\"/lkmenu help\" §afor more §linformation.");
+        }else if(args[0] == "help"){
+            if(sender.hasPermission("lkmenu.help")){
+                sender.sendMessage("§l§7--- §6LKMenu Help §7---");
+                sender.sendMessage("§6/openmenu <name>  §7-- §aOpen a menu.");
+                sender.sendMessage("§6/lkmenu [version]  §7-- §aShow version.");
+                sender.sendMessage("§6/lkmenu help  §7-- §aShow this message.");
+                sender.sendMessage("§6/lkmenu open <name>  §7-- §aSee §6\"/openmenu <name>\"§a.");
+                sender.sendMessage("§6/lkmenu reload  §7-- §aReload config. ");
+            }else{
+                sender.sendMessage("§r[§cLKMenu§r] §cYou don't have permission to do that!");
+            }
+        }else{
+            sender.sendMessage("§r[§cLKMenu§r] §cUnknown command! Type §6\"/lkmenu help\"§c for help.");
+        }
+        return false;
+    }
+}
