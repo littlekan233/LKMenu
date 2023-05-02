ef37bd28aaaea8a110f9926936b941e9629894b5
littlekan233 <little-kan@qq.com>
2023-05-02 12:09:51 +0800

    fix listener

--- src/main/java/ml/littlekan/lkmenu/MenuEvent.java
+++ src/main/java/ml/littlekan/lkmenu/MenuEvent.java
@@ -12,39 +12,42 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MenuEvent implements Listener {
     @EventHandler
     public void onItemClick(InventoryClickEvent inv){
-        JavaPlugin instance = JavaPlugin.getPlugin(LKMenu.class);
-        String[] menus = (String[]) instance.getConfig().get("enabled-menu");
-        MenuJSONTemplate curinv = null;
-        for (String menu : menus) {
-            try {
-                curinv = new Loader().load(menu);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        try {
+            JavaPlugin instance = JavaPlugin.getPlugin(LKMenu.class);
+            String[] menus = (String[]) instance.getConfig().get("enabled-menu");
+            MenuJSONTemplate curinv = null;
+
+            for (String menu : menus) {
+                try {
+                    curinv = new Loader().load(menu);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    if (inv.getWhoClicked().getOpenInventory().getTitle() == curinv.getTitle()) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
+            inv.setCancelled(true);
+            if (inv.getRawSlot() > inv.getInventory().getSize() || inv.getRawSlot() < 0) {
+                return;
             }
 
-            try {
-                if(inv.getWhoClicked().getOpenInventory().getTitle() == curinv.getTitle()){
-                    break;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return;
-        }
-        inv.setCancelled(true);
-        if(inv.getRawSlot() > inv.getInventory().getSize() || inv.getRawSlot() < 0){
-            return;
-        }
-
-        ItemJson[] items = curinv.getItems();
-        for (ItemJson item : items) {
-            if(inv.getRawSlot() == item.getIndex()){
-                Player player = (Player) inv.getWhoClicked();
-                for(String command : item.getClickEvent()){
-                    player.chat(command);
+            ItemJson[] items = curinv.getItems();
+            for (ItemJson item : items) {
+                if (inv.getRawSlot() == item.getIndex()) {
+                    Player player = (Player) inv.getWhoClicked();
+                    for (String command : item.getClickEvent()) {
+                        player.chat(command);
+                    }
                 }
             }
-        }
+        }catch (Exception e){}
         return;
     }
 }
