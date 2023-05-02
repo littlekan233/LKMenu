ef71fcf3ea20e024a279cec9e5e757fb02c42ffa
一个小阚阚 <little-kan@qq.com>
2023-05-02 13:20:05 +0000

    fix gson throws error

--- src/main/java/ml/littlekan/lkmenu/MenuJSONTemplate.java
+++ src/main/java/ml/littlekan/lkmenu/MenuJSONTemplate.java
@@ -5,17 +5,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.google.gson.annotations.SerializedName;
 
 public class MenuJSONTemplate {
-    private JavaPlugin instance = JavaPlugin.getPlugin(LKMenu.class);
-
     @SerializedName("TITLE")
     private String title = "Title";
     @SerializedName("ITEMS")
     private ItemJson[] items;
     @SerializedName("WIDTH")
-    private int width = instance.getConfig().getInt("default-width");
+    private int width = JavaPlugin.getPlugin(LKMenu.class).getConfig().getInt("default-width");
     @SerializedName("HEIGHT")
-    private int height = instance.getConfig().getInt("default-height");
-    
+    private int height = JavaPlugin.getPlugin(LKMenu.class).getInt("default-height");
+
     public String getTitle() {
         return title;
     }
